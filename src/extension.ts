4b4475ef4354a068470d44ad5f415f2abafdf97a
Vitaly <vital2580@icloud.com>
2022-04-24 04:35:15 +0300

    feat: enable uri handler for vue projects only (projects that have package.json with vue dep in root)

--- src/extension.ts
+++ src/extension.ts
@@ -3,17 +3,17 @@ import { readPackageJsonFile } from 'typed-jsonfile'
 
 export const activate = async () => {
     // TODO support multiroot
-    // let hasVue = false
-    // for (const workspaceFolder of vscode.workspace.workspaceFolders ?? []) {
-    //     const packageJson = await readPackageJsonFile({ dir: workspaceFolder.uri.fsPath }).catch(() => null)
-    //     if (!packageJson) continue
-    //     if (Object.keys({ ...packageJson.devDependencies, ...packageJson.dependencies }).includes('vue')) {
-    //         hasVue = true
-    //         break
-    //     }
-    // }
+    let hasVue = false
+    for (const workspaceFolder of vscode.workspace.workspaceFolders ?? []) {
+        const packageJson = await readPackageJsonFile({ dir: workspaceFolder.uri.fsPath }).catch(() => null)
+        if (!packageJson) continue
+        if (Object.keys({ ...packageJson.devDependencies, ...packageJson.dependencies }).includes('vue')) {
+            hasVue = true
+            break
+        }
+    }
 
-    // if (!hasVue) return
+    if (!hasVue) return
     vscode.window.registerUriHandler({
         // vscode://zardoy.vue-devtools-adapter/open-vue?file=src/components/header-profile/index.vue
         async handleUri(uri) {
